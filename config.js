eb36c689626e6842e0be7aa7aedb60719c2bbcbf
Kapil Khurana <kapilkhurana89@gmail.com>
2022-07-10 21:02:25 +0530

    after making config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+// mongoose.createConnection("mongodb://loclhost:27017/e-comm")
+mongoose.connect("mongodb://localhost:27017/e-comm");
+
+// mongoose.connect("mongodb://127.0.0.1/e-comm")
